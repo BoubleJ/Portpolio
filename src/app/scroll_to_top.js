ef0861cd2f5bf3a563a3ac4767edd8d2c9a89404
jaejungbyun <dogmnil2007@naver.com>
2024-01-19 02:33:11 +0900

    commit

--- src/app/scroll_to_top.js
+++ src/app/scroll_to_top.js
@@ -1,15 +1,19 @@
-"use client";
+'use client'
 
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 export default function ScrollToTopButton() {
-  const scrollToTop = () => {
+  const isBrowser = () => typeof window !== "undefined"; //The approach recommended by Next.js
+
+  function scrollToTop() {
+    if (!isBrowser()) return;
     window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+  }
+
   return (
     <>
       <div
-        className="w-12 h-12 border-solid border-4 border-black fixed right-40 bottom-10 "
+        className="w-12 h-12 border-solid border-4 border-black fixed right-40 bottom-10 z-20"
         onClick={scrollToTop}
       >
         <ArrowUpwardIcon fontSize="large" className="w-2/4 m-auto block" />
